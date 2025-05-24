ec7185dda5c8ee8f84059850584c7eeec463f887
Kirill1018 <kirillfedorov031@yandex.ru>
2025-05-24 12:44:21 +0300

    intro to storage data

--- app/src/main/java/com/example/topacademy_android/MainActivity.kt
+++ app/src/main/java/com/example/topacademy_android/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.topacademy_android
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.topacademy_android.databinding.ActivityMainBinding
 import com.google.android.material.button.MaterialButton
@@ -14,32 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.i(null, "program's created")
         val matButt: MaterialButton = findViewById(R.id.material)//updated material styles
         matButt.setOnClickListener { startActivity(Intent(this, HomeActivity::class.java).apply {  }) }
-    }
-    override fun onStart() {
-        super.onStart()
-        Log.i(null, "program's started")
-    }
-    override fun onRestart() {
-        super.onRestart()
-        Log.i(null, "program's restarted")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.i(null, "program's resumed")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.i(null, "program's paused")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.i(null, "program's stopped")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(null, "program's destroyed")
     }
 }
